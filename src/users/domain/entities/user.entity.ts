edf6dca001b3d357c6662146d1ab9e4f9271c5c7
Franchy Rech <franchyrech@gmail.com>
2025-01-14 23:38:43 -0300

    Update method

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -13,12 +13,28 @@ export class UserEntity extends Entity<UserProps> {
     this.props.createdAt = this.props.createdAt ?? new Date()
   }
 
+  update(value: string): void {
+    this.name = value
+  }
+
+  updatePassword(value: string): void {
+    this.password = value
+  }
+
   get name() {
     return this.props.name
   }
 
+  private set name(value: string) {
+    this.props.name = value
+  }
+
   get email() {
     return this.props.email
+  }
+
+  private set password(value: string) {
+    this.props.password = value
   }
 
   get password() {
